a74bda1800c6eaab377bfc9ee2845effb8ce38b4
Yoru <m.f.fetvaci@hotmail.com>
2024-04-28 20:10:54 +0300

    /token api endpoint

---
+++ api/token.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"fun-api/utils"
+	"net/http"
+	"os"
+)
+
+func Token(w http.ResponseWriter, r *http.Request) {
+	secret := r.URL.Query().Get("secret")
+	if secret != os.Getenv("secret") {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "Internal Server Error"}`))
+		return
+	}
+
+	payloadData := map[string]interface{}{
+		"grant_type":    "client_credentials",
+		"client_id":     os.Getenv("client_id"),
+		"client_secret": os.Getenv("client_secret"),
+		"scope":         "public",
+		"code":          "code",
+	}
+
+	payloadBytes, err := json.Marshal(payloadData)
+	if err != nil {
+		fmt.Println("Error marshalling JSON:", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "Internal Server Error"}`))
+		return
+	}
+
+	data, err := utils.Post("https://osu.ppy.sh/oauth/token", bytes.NewReader(payloadBytes))
+
+	if err != nil {
+		fmt.Println("Error fetching OAuth token:", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "Failed to fetch OAuth token"}`))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
