f6a09f7dc29a061eb5411e632fd47dda8f029215
Yoru <m.f.fetvaci@hotmail.com>
2024-04-28 20:11:05 +0300

    reformat /user endpoint

--- api/user.go
+++ api/user.go
@@ -1,8 +1,8 @@
 package api
 
 import (
-	"encoding/json"
 	"fmt"
+	"fun-api/utils"
 	"net/http"
 	"os"
 )
@@ -11,8 +11,6 @@ func User(w http.ResponseWriter, r *http.Request) {
 	username := r.URL.Query().Get("username")
 	mode := r.URL.Query().Get("mode")
 
-	token := os.Getenv("token")
-
 	if username == "" || mode == "" {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusBadRequest)
@@ -20,7 +18,13 @@ func User(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data, err := fetchUserData(username, mode, token)
+	headers := map[string]string{
+		"Content-Type":  "application/json",
+		"Accept":        "Accept: application/json",
+		"Authorization": "Bearer " + os.Getenv("access_token"),
+	}
+
+	data, err := utils.Get(fmt.Sprintf("https://osu.ppy.sh/api/v2/users/%s/%s", username, mode), headers)
 
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json")
@@ -30,34 +34,7 @@ func User(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
-}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(data)
 
-func fetchUserData(username string, mode string, token string) (map[string]interface{}, error) {
-	reqUrl := fmt.Sprintf("https://osu.ppy.sh/api/get_user?u=%s&m=%s&k=%s", username, mode, token)
-
-	req, err := http.NewRequest("GET", reqUrl, nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	var userData []map[string]interface{}
-	err = json.NewDecoder(resp.Body).Decode(&userData)
-	if err != nil {
-		return nil, err
-	}
-
-	if len(userData) > 0 {
-		return userData[0], nil
-	}
-
-	return nil, fmt.Errorf("no user data found")
 }
--- main.go
+++ main.go
@@ -7,18 +7,46 @@ import (
 	"fun-api/utils"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/juju/ratelimit"
 )
 
+var limiter = ratelimit.NewBucketWithRate(100, 100)
+var baseUrl = "http://localhost:3000"
+
 func main() {
+	go refreshToken()
+
 	http.HandleFunc("/", handlers.Index)
 	http.HandleFunc("/mood", handlers.Mood)
 	http.HandleFunc("/hello", handlers.Hello)
 	http.HandleFunc("/card", handlers.Card)
 
 	http.HandleFunc("/api/user", api.User)
+	http.HandleFunc("/api/token", func(w http.ResponseWriter, r *http.Request) {
+		if limiter.TakeAvailable(1) == 0 {
+			http.Error(w, "Rate limit exceeded", http.StatusTooManyRequests)
+			return
+		}
+		api.Token(w, r)
+	})
 
 	http.HandleFunc("/media/", utils.MediaRedirector)
 
 	fmt.Println("Listening on http://localhost:3000")
 	log.Fatal(http.ListenAndServe(utils.GetPort("3000"), nil))
+}
+
+func refreshToken() {
+	secret := os.Getenv("secret")
+
+	utils.SetToken(secret, baseUrl)
+
+	ticker := time.NewTicker(12 * time.Hour)
+	defer ticker.Stop()
+	for range ticker.C {
+		utils.SetToken(secret, baseUrl)
+	}
 }
