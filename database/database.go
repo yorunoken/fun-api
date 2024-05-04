005a1ce8c4df627f1a6d23b655b7ad5d60e18bb9
Yoru <m.f.fetvaci@hotmail.com>
2024-05-04 19:34:50 +0300

    add database helpers

---
+++ database/database.go
@@ -0,0 +1,69 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Data struct {
+	Key   string
+	Value string
+}
+
+func EntryExists(db *sql.DB, table string, id string) bool {
+	var count int
+	db.QueryRow(fmt.Sprintf("SELECT * FROM %s WHERE id = ?", table), id).Scan(&count)
+
+	return count > 0
+}
+
+func GetEntry(db *sql.DB, table string, id string) (*sql.Rows, error) {
+	return db.Query(fmt.Sprintf("SELECT * FROM %s WHERE id = ?", table), id)
+}
+
+func AddEntry(db *sql.DB, table string, id string, dataArr []Data) (sql.Result, error) {
+	setClause := ""
+	values := []string{}
+
+	for index, data := range dataArr {
+		values = append(values, data.Value)
+		if index == len(dataArr)-1 {
+			setClause += data.Key + " = ?"
+		} else {
+			setClause += data.Key + " = ?, "
+		}
+	}
+
+	valueArgs := make([]interface{}, len(values))
+	for i, v := range values {
+		valueArgs[i] = v
+	}
+
+	rowExists := EntryExists(db, table, id)
+	if !rowExists {
+		fields := []string{}
+		placeholders := ""
+
+		for index, data := range dataArr {
+			fields = append(fields, data.Key)
+
+			if index == len(dataArr)-1 {
+				placeholders += "?"
+			} else {
+				placeholders += "?, "
+			}
+
+		}
+
+		args := append([]interface{}{id}, valueArgs...)
+		query := fmt.Sprintf("INSERT INTO %s (id, %s) values (?, %s)", table, strings.Join(fields, ", "), placeholders)
+		return db.Exec(query, args...)
+	}
+
+	args := append(valueArgs, id)
+	query := fmt.Sprintf("UPDATE %s SET %s WHERE ID = ?", table, setClause)
+	return db.Exec(query, args)
+}
