f878791c5931a9a42e9ef238493d1f5b8b63faf0
Yoru <m.f.fetvaci@hotmail.com>
2024-05-05 00:10:14 +0300

    Update error.go

--- utils/error.go
+++ utils/error.go
@@ -8,5 +8,6 @@ import (
 func WriteError(w http.ResponseWriter, err string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusBadRequest)
+	fmt.Println(err)
 	w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, err)))
 }
